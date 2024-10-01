450f9098aeb4304d8108fd10aecc414c86eb19d8
edub4 <edusantos160506@gmail.com>
2024-09-30 22:12:25 -0300

    tela com input de nome, numero e cpf.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textS}>Seja Bem-vindo</Text>
+      <Text >Digite seu Nome</Text>
+      <TextInput placeholder='nome' style={styles.textInp}/>
+      <Text>Digite seu Telefone</Text>
+      <TextInput placeholder='(11)99999 9999' style={styles.textInp}/>
+      <Text>Digite seu CPF</Text>
+      <TextInput placeholder='000.000.000-00' style={styles.textInp}/>
+      <TouchableOpacity activeOpacity={0.7} style={styles.btn}>
+        <Text>Enviar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#808',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInp:{
+    backgroundColor: '#fff',
+    width: 300,
+    textAlign:'center',
+    borderRadius: 5,
+    margin: 10,
+  },
+  textS:{
+    margin: 10,
+    fontSize: 30,
+  },
+  btn: {
+    backgroundColor:'rgb(192,192,192)',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+    width: 200
+  }
 });
