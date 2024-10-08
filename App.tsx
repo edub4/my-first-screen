8069d92ad6a14e0469d28fc68be104353f019a4f
edub4 <edusantos160506@gmail.com>
2024-10-07 21:54:37 -0300

    trabalhando com hook

--- App.tsx
+++ App.tsx
@@ -1,19 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [newTask, setNewTask] = React.useState('');
+
   return (
     <View style={styles.container}>
+      {/*
       <Text style={styles.textS}>Seja Bem-vindo</Text>
       <Text >Digite seu Nome</Text>
       <TextInput placeholder='nome' style={styles.textInp}/>
+
       <Text>Digite seu Telefone</Text>
       <TextInput placeholder='(11)99999 9999' style={styles.textInp}/>
+
       <Text>Digite seu CPF</Text>
       <TextInput placeholder='000.000.000-00' style={styles.textInp}/>
+
       <TouchableOpacity activeOpacity={0.7} style={styles.btn}>
-        <Text>Enviar</Text>
+        <Text>Adicionar tarefa</Text>
       </TouchableOpacity>
+      */}
+      <Text>Trabalhando com Hook</Text>
+      <TextInput onChangeText={setNewTask} placeholder='tarefa' style={styles.textInp}/>
+      <Text> ITENS ADICIONADOS</Text>
+      <Text>{newTask}</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
